070febbc1a65ddae99a605786d46ebc922266bc9
Adimchi <oraegbunamadimchi@gmail.com>
2022-09-20 00:32:04 +0100

    added flex to navbar

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import roman from '../images/roman.png'
+
+const main = () => {
+  return (
+    <>
+    <main className='w-full mt-5'>
+     
+        
+        <div className=' pill bg-primary'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nostrum ipsum, eum doloribus consequuntur reprehenderit nesciunt illum iste! Unde corporis veniam voluptatibus ducimus obcaecati? Aliquam, exercitationem! Obcaecati iure doloremque excepturi.
+ </div>
+    <div className='' >
+<img src={roman} className="  border-2 border-dashed p-12 w-96 rounded-full " alt="White digital scale dispaying 650 " />
+    </div>
+    <div> </div>
+    </main>
+    </>
+  )
+}
+
+export default main
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 const Nav = () => {
   return (
-   <nav className=' w-full '>
-    <a className=" w-1/5 float-none  md:float-left text-left block p-12 md:text-center font-extrabold lg:text-xl" href="#">OUTLIVE</a>
-  <a href="#" className='w-1/5 float-none  md:float-left text-left block p-12 md:text-center'>Home</a>
-  <a href="#"className='w-1/5 float-none  md:float-left text-left block p-12 md:text-center ' >Services</a>
-  <a href="#"className='w-1/5 float-none  md:float-left text-left block p-12 md:text-center' >Contact</a>
-  <a href="#"className='w-1/5 float-none  md:float-left text-left block p-12 md:text-center' >Get The App</a>
+   <nav className=' flex justify-around mx-auto w-full mt-6  text-white'>
+    <a className=" w-1/5  md:text-center font-extrabold lg:text-xl" href="#">OUTLIVE</a>
+  
+      <div>
+  <a href="#" className='w-1/5 md:text-center'>Home</a>
+  </div>
+  <div>
+  <a href="#"className='w-1/5   md:text-center ' >Services</a>
+  </div>
+  <div>
+  <a href="#"className='w-1/5     md:text-center' >Contact</a>
+  </div>
+  <div className=' bg-primary px-5 py-2'>
+  <a href="#"className='w-1/5   md:text-center' >Get The App</a>
+  </div>
+ 
    </nav>
   )
 }
