9e9e35a06eb45f1ea6db6ba6d255aa64776d2691
Adimchi <oraegbunamadimchi@gmail.com>
2022-10-04 14:50:54 +0100

    added images to grid

---
+++ src/components/pages/Panel.jsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import sag from '../images/sag.jpeg'
+// import bret from '../images/bret.jpeg'
+// import gig from '../images/gig.jpeg'
+
+
+// const Panel = () => {
+//   return (
+//     <div>
+//         <div className="panels mx-10  lg:mx-20 mt-10 grid md:grid-cols-2 lg:grid-cols-4 gap-5">
+//     <div className=" ">
+//         <img src={sag} alt="HD psychedellic" className='shadow-xl' />
+//     </div>
+//     <div className=" shadow ">
+//         <img src={bret} alt=" gold and pink dragon illustration "/>
+//   </div>
+//     <div className=" shadow p-4">
+//    <img src={gig} alt="black and white Optical illusion" /> </div>
+//     <div className=" shadow p-4">
+//       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, eaque corrupti maxime eius sit qui! Quisquam labore ipsa officiis, quidem ab minus itaque, dolore ea modi illo commodi quo odit.
+//     </div>
+//    </div>
+//    </div>
+//   )
+// }
+
+// export default Panel
