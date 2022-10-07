1ee17858010307ed3fb7a7bca79de000ddbaee1d
Adimchi <oraegbunamadimchi@gmail.com>
2022-10-07 16:26:27 +0100

    added disclosure component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,26 +5,89 @@ import indi from './images/indi.png'
 import rose from './images/rose.png'
 import './styles/Content.css'
 import { Disclosure } from '@headlessui/react'
+import { useState } from 'react'
 
 const Content = () => {
+
   return (
+    
     <div className='relative grid gap-4 md:grid-cols-2 lg:grid-cols-4 p-12'>
         <div className='weed  drop-shadow-2xl'>
 <img src={yum} alt="" className='mx-auto mt-24' />
+<div className="  text-white mx-5 mb-5 rounded-lg px-3 bg-green-900 w-48">
+<Disclosure>
+      <Disclosure.Button className="py-2">
+      <div className='flex items-center'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 mr-1 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+</svg>
 
+<span className=' tracking-widest'>Mary joe</span>
+  </div>
+      </Disclosure.Button>
+      <Disclosure.Panel className="">
+      <span>THC - 18% || CBD - 3%  </span>
+      </Disclosure.Panel>
+    </Disclosure>
+    </div>
 </div>
 <div className='weed drop-shadow-2xl'>
-<img src={rose} alt=""  />
+<img src={rose} alt="" className=' mt-20'  />
+<div className="text-white mx-5 mb-5 rounded-lg px-3 bg-green-900 w-48">
+<Disclosure>
+      <Disclosure.Button className="py-2">
+      <div className='flex items-center'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 mr-1 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+</svg>
 
+<span className=' tracking-widest'>Wedding cake </span>
+  </div>
+      </Disclosure.Button>
+      <Disclosure.Panel className="">
+      <span>THC - 10% || CBD - 12% </span>
+      </Disclosure.Panel>
+    </Disclosure>
+    </div>
 </div>
 <div className='weed drop-shadow-2xl'>
-<img src={thc} alt="" className=' mt-24' />
-
+<img src={thc} alt="" className=' mt-20' />
+<div className="text-white mx-5 mb-5 rounded-lg px-3 bg-green-900 w-48">
+<Disclosure>
+      <Disclosure.Button className="py-2">
+      <div className='flex items-center'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 mr-1 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+</svg>
+<span className=' tracking-widest'>Krabs</span>
+  </div>
+      </Disclosure.Button>
+      <Disclosure.Panel className="">
+      <span>THC - 20% || CBD - 3% </span>
+      </Disclosure.Panel>
+    </Disclosure>
+    </div>
 </div>
+
 <div className='weed drop-shadow-2xl'>
-<img src={indi} alt="" className=' mt-16'  />
-
+<img src={indi} alt="" className=''  />
+<div className="text-white mx-5 mb-5 rounded-lg px-3 bg-green-900 w-48">
+<Disclosure>
+      <Disclosure.Button className="py-2">
+      <div className='flex items-center'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 mr-1 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+</svg>
+<span className=' tracking-widest'>Snell</span>
+  </div>
+      </Disclosure.Button>
+      <Disclosure.Panel className="">
+      <span>THC - 18% </span>
+      </Disclosure.Panel>
+    </Disclosure>
+    </div>
 </div>
+
     </div>
   )
 }
--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -4,15 +4,15 @@ import master from './images/master.png'
 const Master = () => {
   return (
     <div className='bg-black mt-32 lg:p-5 flex flex-col md:flex-row'>
-         <div className='w-full  md:hidden  lg:w-1/2 '>
+         <div className='w-full  md:hidden '>
     <img src={master} alt="Happy by Manuel Cetina" />
     </div>
         <div className='md:w-1/2 lg:p-12 w-full p-5 text-white  '>
     <h2 className='text-5xl '>Its Master Kush's World. We just live in it!</h2>
     {/* From flowery.co */}
-    <p>F%$k corporate cannabis, that’s not us. We had to fight for our existence from the first moment we decided to pursue an MMTC license, and after years of litigation with the state, we were finally cleared for take off and able to spread our wings. We’re all consumers, and that familiarity with the plant, the community and the struggle over the years is close to our hearts. we also understand the culture and appreciate the need for transparency in this industry.</p>
+    <p className=' py-10'>F%$k corporate cannabis, that’s not us. We had to fight for our existence from the first moment we decided to pursue an MMTC license, and after years of litigation with the state, we were finally cleared for take off and able to spread our wings. We’re all consumers, and that familiarity with the plant, the community and the struggle over the years is close to our hearts. we also understand the culture and appreciate the need for transparency in this industry.</p>
     </div>
-    <div className='w-full hidden md:block lg:blovk lg:w-1/2 '>
+    <div className=' hidden md:block w-1/2 '>
     <img src={master} alt="Happy by Manuel Cetina" />
     </div>
     </div>
