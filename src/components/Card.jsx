e4b4ec4ed7d2f442bca7f78a55681a5e3c18e47c
Adimchi <oraegbunamadimchi@gmail.com>
2022-10-10 20:51:02 +0100

    added animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import flower from './images/flower.png'
+import { motion, useScroll } from 'framer-motion';
+
 
 const Card = () => {
   return (
     <div>
        <div className='bg-black mt-32 lg:p-5 flex flex-col md:flex-row'>
          <div className='w-full md:w-1/2 '>
-    <img src={flower} alt="Happy by Manuel Cetina" />
+    <motion.img src={flower} alt="Happy by Manuel Cetina" 
+    initial={{ opacity: 0.1, scale: 0.5, y:'-100vw' }}
+     animate={{ opacity: 1, scale: 1, y: 0 }}
+     transition={{
+       duration: 1,
+       delay: 0.5,
+       ease: [0, 0.71, 0.2, 1.01]
+     }}/>
     </div>
-        <div className='md:w-1/2 mt-16 lg:p-12 w-full p-5 text-white  '>
+        <motion.div className='md:w-1/2 mt-16 lg:p-12 w-full p-5 text-white'
+        initial={{ opacity: 0.1, scale: 0.5, x:'100vw' }}
+         animate={{ opacity: 1, scale: 1, x: 0 }}
+         transition={{
+           duration: 1,
+           delay: 0.5,
+           ease: [0, 0.71, 0.2, 1.01]
+         }}>
     <h2 className='text-5xl '>You know what time is it!!!</h2>
     {/* From flowery.co */}
     <p className=' note py-10'>
     We’re here to shed light on the often confusing world of cannabis. Because we believe that this incredible plant holds the potential to not only help relieve chronic medical issues, but also be a part of one’s everyday wellness routine. No matter what effect you’re after, we are dedicated to helping you unlock all the benefits the cannabis plant can provide.
 </p>
-    </div>
+    </motion.div>
   
     </div>
     </div>
--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -21,7 +21,13 @@ const Master = () => {
     <p className=' note py-10'>F%$k corporate cannabis, that’s not us. We had to fight for our existence from the first moment we decided to pursue an MMTC license, and after years of litigation with the state, we were finally cleared for take off and able to spread our wings. We’re all consumers, and that familiarity with the plant, the community and the struggle over the years is close to our hearts. we also understand the culture and appreciate the need for transparency in this industry.</p>
     </motion.div>
     <div className=' hidden md:block md:mt-12 w-1/2 '>
-    <img src={master} alt="Happy by Manuel Cetina" />
+    <motion.img src={master} alt="Happy by Manuel Cetina"    initial={{ opacity: 0.1, scale: 0.5, y:'-100vw' }}
+      animate={{ opacity: 1, scale: 1, y: 0 }}
+      transition={{
+        duration: 1,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }} />
     </div>
     </div>
   )
