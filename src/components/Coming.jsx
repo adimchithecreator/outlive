d9cb7bf90ef4f8843603906182f233b2ce7653e5
Adimchi <oraegbunamadimchi@gmail.com>
2022-10-12 22:01:47 +0100

    restructured layout

--- src/components/Coming.jsx
+++ src/components/Coming.jsx
@@ -3,12 +3,12 @@ import simon from './images/simon.jpeg'
 
 const Coming = () => {
   return (
-    <div className='  flex text-white '>
-<div className='bg-black w-1/2 text-7xl px-5 py-32'>
+    <div className='  flex flex-col md:flex-row  text-white '>
+<div className='bg-black md:w-1/2 text-7xl px-5 py-32'>
     Coming soon to a dispensary near you!
 </div>
-   <div className=" w-1/2">
-    <img src={simon} alt="" />
+   <div className=" md:w-1/2">
+    <img src={simon} alt="" className=' h-full' />
    </div>
     </div>
   )
