6f004191d2f97ae37c7bb335b517dbaf1edc6cb3
Adimchi <oraegbunamadimchi@gmail.com>
2022-10-13 14:36:02 +0100

    changed layout from flex to grid

--- src/components/Coming.jsx
+++ src/components/Coming.jsx
@@ -3,13 +3,13 @@ import simon from './images/simon.jpeg'
 
 const Coming = () => {
   return (
-    <div className='  flex flex-col md:flex-row  text-white '>
-<div className='bg-black md:w-1/2 text-7xl px-5 py-32'>
+    <div className=' grid md:grid-cols-2 text-white '>
+<div className='bg-black  text-7xl px-5 py-32'>
     Coming soon to a dispensary near you!
 </div>
-   <div className=" md:w-1/2">
-    <img src={simon} alt="" className='' />
-   </div>
+
+    <img src={simon} alt="" className=' md:h-full' />
+ 
     </div>
   )
 }
